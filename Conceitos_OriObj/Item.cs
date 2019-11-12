25ed22168dcf072ceea4e90289a08e6fa5f6dbaf
HawksDeath <jefferson.felicio@hotmail.com>
2019-11-12 20:02:13 -0300

    Denovo

---
+++ Conceitos_OriObj/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Conceitos_OriObj
+{
+    class Item
+    {
+        //Instancias de classe (field)
+        public string NomeItem;
+        public double ValorItem;
+        public int QuantidadeItem;
+
+        //Public ou private sao modificadores de acesso
+
+        public double AtualizaInventario() //Essa linha inteira do metodo se chama: Assinatura do Metodo
+        {
+            return ValorItem * QuantidadeItem;
+        }
+
+        public void AdicionaItens(int quantidade)
+        {
+            QuantidadeItem += quantidade;
+        }
+
+        public void RemoveItens(int quantidade)
+        {
+            QuantidadeItem -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return NomeItem
+                + ", $ "
+                + ValorItem.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + QuantidadeItem
+                + " unidades, Total: $"
+                + AtualizaInventario().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Conceitos_OriObj/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Globalization;
+
+
+namespace Conceitos_OriObj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar a classe Item
+            Item i = new Item();
+
+            WriteLine("Entre com os dados do Item: ");
+            Write("Nome do Item: ");
+            i.NomeItem = ReadLine();
+
+            Write("Valor do Item: ");
+            i.ValorItem = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+            Write("Quantidade no inventario: ");
+            i.QuantidadeItem = int.Parse(ReadLine());
+
+            WriteLine();
+            Write("Dados do Item: " + i);
+
+            WriteLine("");
+            Write("Digite o numero de itens a ser adicionado ao inventario: ");
+            int qtdA = int.Parse(ReadLine());
+            i.AdicionaItens(qtdA);
+
+            WriteLine("");
+            WriteLine("Dados atualizados: " + i);
+
+            WriteLine("");
+            Write("Digite o numero de itens a ser removido do inventario: ");
+            int qtdR = int.Parse(ReadLine());
+            i.RemoveItens(qtdR);
+
+            WriteLine("");
+            WriteLine("Dados atualizados: " + i);
+            
+
+            WriteLine("");
+        }
+    }
+}
